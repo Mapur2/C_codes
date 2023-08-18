bc110d6a1f8acb867278e31bf4040ee7000d5a88
Rupam Modak <rupammodak1@gmail.com>
2023-08-18 07:41:57 +0530

    Add files via upload

--- Basic DSA(Linked list)/push_sort.c
+++ Basic DSA(Linked list)/push_sort.c
@@ -9,6 +9,66 @@ struct link{
 typedef struct link node;
 node* pushSort(node *head,int e)
 {
+	node *ele,*h,*p;
+	int f=0;
+	ele=(node *)malloc(sizeof(node));
+	ele->n=e;
+	ele->next=NULL;
+	if(head==NULL)
+	{
+		head=ele;
+	}
+	else
+	{
+		if(e<head->n){
+			ele->next=head;
+			head=ele;
+		}
+		else{
+			h=head;
+			while(h!=NULL && h->n<=e){
+				p=h;
+				h=h->next;
+			}
+			ele->next=p->next;
+			p->next=ele;
+		}
+	}
+	return head;
+}
+void main()
+{
+	node *head,*t;
+	head=NULL;
+	int ch,e,f=0;
+	while(f==0)
+	{
+		printf("enter 1 for push , 2 for exit ");
+		scanf("%d",&ch);
+		switch(ch)
+		{
+			case 1:
+				printf("enter the number ");
+				scanf("%d",&e);
+				head=pushSort(head,e);
+				break;
+			case 2:
+				f=1;
+				break;
+		}
+		t=head;
+		while(t!=NULL)
+		{
+			printf("%d ",t->n);
+			t=t->next;
+		}
+		printf("\n");
+	}
+}
+/*
+This is an alternative way but has more time complexity
+node* pushSort(node *head,int e)
+{
 	node *ele,*h,*t;
 	int f=0;
 	ele=(node *)malloc(sizeof(node));
@@ -47,33 +107,4 @@ node* pushSort(node *head,int e)
 		}
 		return head;
 	}
-}
-void main()
-{
-	node *head,*t;
-	head=NULL;
-	int ch,e,f=0;
-	while(f==0)
-	{
-		printf("enter 1 for push , 2 for exit ");
-		scanf("%d",&ch);
-		switch(ch)
-		{
-			case 1:
-				printf("enter the number ");
-				scanf("%d",&e);
-				head=pushSort(head,e);
-				break;
-			case 2:
-				f=1;
-				break;
-		}
-		t=head;
-		while(t!=NULL)
-		{
-			printf("%d ",t->n);
-			t=t->next;
-		}
-		printf("\n");
-	}
-}
+}*/
